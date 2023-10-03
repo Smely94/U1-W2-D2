3f39c2408c1f0a45d52cdb125067cf8ddee3ae25
Smely94 <compagnonisamuele@gmail.com>
2023-10-03 18:00:29 +0200

    modifica ecercise

--- D2.js
+++ D2.js
@@ -7,7 +7,9 @@
 let num1 = 10;
 let num2 = 20;
 if (num2 > num1) {
-  console.log(num2);
+  console.log("num2 è maggiore di num1");
+} else if (num1 > num2) {
+  console.log("num1 è maggiore di num2");
 }
 
 /* ESERCIZIO 2
@@ -65,6 +67,17 @@ if (totalShoppingCart >= 50) {
 
 /* SCRIVI QUI LA TUA RISPOSTA */
 
+const totalShoppingCart2 = 90;
+
+let discountshopping = totalShoppingCart2 * 0.8;
+console.log(discountshopping);
+
+if (discountshopping >= 50) {
+  console.log("Complimenti! La tua spedizione è gratuita");
+} else {
+  console.log(discountshopping + 10, "Mi dispiace. Paghi 10 di spedizione");
+}
+
 /* ESERCIZIO 7
   Crea tre variabili, e assegna un valore numerico a ciascuna di esse.
   Utilizzando un blocco condizionale, crea un algoritmo per ordinarle secondo il loro valore, dal più alto al più basso.
@@ -77,10 +90,12 @@ num4 = 100;
 num5 = 80;
 num6 = 120;
 
-if (num1 > num5 > num6) {
+if (num4 > num5 && num5 > num6) {
   console.log(num4, num5, num6);
-} else if (num6 > num5 > num4) {
+} else if (num6 > num5 && num5 > num4) {
   console.log(num6, num5, num4);
+} else if (num4 > num5 && num6 > num5) {
+  console.log(num6, num4, num5);
 }
 
 /* ESERCIZIO 8
